2d9ac267e84eb14da96df714dd4a6e409a05cdcc
ken-zlai <ken@zipline.ai>
2024-09-18 09:37:49 -0600

    prediction drift line chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
 <a href="/viz/heatmap">heatmap</a>
+<a href="/viz/prediction_drift">prediction_drift</a>
+
+<style>
+	a {
+		display: block;
+	}
+</style>
---
+++ src/routes/viz/prediction_drift/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as echarts from 'echarts';
+
+	const xAxis = [
+		'01-01-2024',
+		'01-02-2024',
+		'01-03-2024',
+		'01-04-2024',
+		'01-05-2024',
+		'01-06-2024',
+		'01-07-2024'
+	];
+
+	const xAxisAnomolies = ['01-03-2024', '01-04-2024'];
+
+	function generateData(xAxis, xAxisAnomolies) {
+		const data = [];
+
+		for (let i = 0; i < xAxis.length; i++) {
+			let z;
+
+			if (xAxisAnomolies.includes(xAxis[i])) {
+				z = Math.random() * 0.2; // random number between 0 and 0.2
+			} else {
+				z = Math.random() * 0.3 + 0.7; // random number between 0.7 and 1
+			}
+			data.push(z);
+		}
+
+		return data;
+	}
+
+	const data = generateData(xAxis, xAxisAnomolies);
+
+	const option = {
+		xAxis: {
+			type: 'category',
+			data: xAxis
+		},
+		yAxis: {
+			type: 'value',
+			name: 'Model Accuracy',
+			nameLocation: 'middle',
+			nameRotate: 90,
+			nameGap: 40, // Adjust the gap between the y-axis labels and the name
+			nameTextStyle: {
+				fontSize: 18 // Optional: Adjust the font size
+			}
+		},
+		series: [
+			{
+				data: data,
+				type: 'line',
+				smooth: true
+			}
+		]
+		// visualMap: {
+		// 	min: 0,
+		// 	max: 1,
+		// 	orient: 'vertical',
+		// 	right: '0'
+		// },
+		// grid: {
+		// 	left: '100px'
+		// }
+	};
+
+	onMount(() => {
+		var chartDom = document.getElementById('prediction-drift');
+		var myChart = echarts.init(chartDom);
+		myChart.setOption(option);
+	});
+</script>
+
+<div id="prediction-drift" style="height: 300px; width: 800px"></div>
