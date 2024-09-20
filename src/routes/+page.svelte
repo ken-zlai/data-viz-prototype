99f38f7044b317ca550eedfd36cfb50a077bd7fa
ken-zlai <ken@zipline.ai>
2024-09-20 14:38:26 -0600

    routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <a href="/viz/heatmap">heatmap</a>
 <a href="/viz/prediction_drift">prediction_drift</a>
 <a href="/viz/distributions">distributions</a>
+<a href="/viz/lines">lines</a>
+<a href="/viz/lines_group_by">lines_group_by</a>
+<a href="/viz/bars">bars</a>
 
 <style>
 	a {
